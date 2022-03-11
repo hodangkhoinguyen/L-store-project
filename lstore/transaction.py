86f485ea0e88939643357a1c1f3edeab191f5c9c
javuong01 <82010935+javuong01@users.noreply.github.com>
2022-03-11 00:44:53 -0800

    Update transaction.py

--- lstore/transaction.py
+++ lstore/transaction.py
@@ -1,3 +1,4 @@
+from ast import Delete
 from lstore.table import Table, Record
 from lstore.index import Index
 from lstore.query import Query
@@ -11,8 +12,6 @@ class Transaction:
     def __init__(self):
         self.queries = []
         self.queryStack = LifoQueue()
-        self.originalStack = LifoQueue()
-        self.tableName = None
         pass
 
     """
@@ -23,38 +22,56 @@ class Transaction:
     # t.add_query(q.update, grades_table, 0, *[None, 1, None, 2, None])
     """
     def add_query(self, query, table, *args):
-        self.queries.append((query, args))
-        if self.tableName == None:
-            self.tableName = table
+        self.queries.append((query, args, table))
+        #if self.tableName == None:
+            #self.tableName = table
         # use grades_table for aborting
 
     # If you choose to implement this differently this method must still return True if transaction commits or False on abort
     def run(self):
         #print("query ran")
-        for query, args in self.queries:
-            # This segment saves original values if it encounters an update operation and finds existing records
-            if len(args) == 2:
-                saveOrignal = Query(self.tableName).currentValues(args[0])
-                if not len(saveOrignal) == 0:
-                    self.originalStack.put(saveOrignal)
+        for query, args, table in self.queries:
 
+            originalVal = []
+            queryName = ""
+            page_directory = []
+
+            
+            if len(args) == 1:
+                queryName = "delete"
+            
+            if len(args) == 2:
+                queryName = "update"
+                # This segment saves original values if it encounters an update operation and finds existing records
+                saveOriginal = Query(table).currentValues(args[0])
+                if not len(saveOriginal) == 0:
+                    originalVal = saveOriginal
+
+            if len(args) == 3:
+                queryName = "skip"
+            
             # This segment gets original RID to be put into stack (not for insert)
             if not len(args) == 5:
-                originalRID = Query(self.tableName).returnRID(args[0])
+                originalRID = Query(table).returnRID(args[0])
+                if not originalRID == None:
+                    page_directory = table.page_directory(originalRID)
 
             # Runs the operation
             result = query(*args)
 
             # This segment gets original RID to be put into stack (for insert)
             if len(args) == 5:
-                originalRID = Query(self.tableName).returnRID(args[0])
+                queryName = "insert"
+                originalRID = Query(table).returnRID(args[0])
+                if not originalRID == None:
+                    page_directory = table.page_directory(originalRID)
             
             # If op fails, it aborts
             if result == False:
                 return self.abort()
             
             # Stack for aborting is appended if needed
-            self.queryStack.put((query, args, originalRID))
+            self.queryStack.put((queryName, args, originalRID, table, originalVal, page_directory))
             #print("stack appended")
 
         return self.commit()
@@ -67,26 +84,31 @@ class Transaction:
         #If stack of ops that went through is not empty, reverse the damage
         while self.queryStack.empty() == False:
             #print("in loop")
-            revQuery = self.queryStack.get()
+            queryName, args, originalRID, table, originalVal, page_directory = self.queryStack.get()
             print(self.queryStack.qsize())
 
-            # This skips select ops
-            if len(revQuery[1]) == 3:
+            # This skips select and aggregate ops
+            if queryName == "skip":
                 continue
 
             # This undos insert with a delete
-            if len(revQuery[1]) == 5:    
-                Query(self.tableName).delete(revQuery[1][0])
+            if queryName == "insert":    
+                Query(table).delete(args[0])
                 print("successful insert abort")
                 continue
 
             # This undos update with an update of the original values
-            if len(revQuery[1]) == 2:    
-                Query(self.tableName).update(revQuery[1][0], self.originalStack.get())
+            if queryName == "update":    
+                Query(table).update(args[0], originalVal)
                 print("successful update abort")
+                continue
 
-        while not self.originalStack.empty():
-            self.originalStack.get() 
+            # This undos delete  (not done yet)
+            if queryName == "delete":
+                print("successful delete abort")
+                continue
+
+
         return False
 
     def commit(self):
@@ -96,7 +118,6 @@ class Transaction:
         # Emptying out the stacks
         while not self.queryStack.empty():
             self.queryStack.get()
-        while not self.originalStack.empty():
-            self.originalStack.get()
+
         return True
 
