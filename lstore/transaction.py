b72d5eb7bb1a0c8b5b9b7154e05643469269a7f7
hodangkhoinguyen <50374959+hodangkhoinguyen@users.noreply.github.com>
2022-03-12 03:47:41 -0800

    Revert "Update transaction.py"
    This reverts commit 6e8393b256cc585904bc34cd7a5a5f0678d6b16c.

--- lstore/transaction.py
+++ lstore/transaction.py
@@ -86,22 +86,24 @@ class Transaction:
 
             # This skips select and aggregate ops
             if queryName == "read":
+                
                 continue
 
             # This undos insert with a delete
             if queryName == "insert":    
-                Query(table).revert_insert(originalRID[0])
+                #Query(table).delete(args[0])
+                
                 continue
 
             # This undos update with an update of the original values
             if queryName == "update":    
-                Query(table).revert_update(originalRID[0], originalVal)
-                print(originalRID, originalVal)
+                #Query(table).update(args[0], originalVal)
+                
                 continue
 
             # This undos delete  (not done yet)
             if queryName == "delete":
-                Query(table).revert_delete(originalRID[0], page_directory)
+                
                 continue
 
 
