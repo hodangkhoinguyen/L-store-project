bc6115fab2468f9c23b957f345faf0bb8219d5dc
aliouziz <88304905+aliouziz@users.noreply.github.com>
2022-03-12 02:41:34 -0800

    Update table.py

--- lstore/table.py
+++ lstore/table.py
@@ -1,15 +1,13 @@
 import threading
 from lstore.index import Index
-from threading import Event, Thread, Timer
+
+from threading import Timer
 import time
-import copy
 INDIRECTION_COLUMN = 0
 RID_COLUMN = 1
 TIMESTAMP_COLUMN = 2
 SCHEMA_ENCODING_COLUMN = 3
 
-
-            
 class RepeatedTimer(object):
   def __init__(self, interval, function, *args, **kwargs):
     self._timer = None
@@ -36,7 +34,7 @@ class RepeatedTimer(object):
   def stop(self):
     self._timer.cancel()
     self.is_running = False
-    
+
 class Record:
 
     def __init__(self, rid, key, columns):
@@ -88,64 +86,47 @@ class Table:
         self.db = None
         self.page_directory = {}        
         self.page_range_list = []
-        self.lock = {}
+        self.lock = []
         self.index = Index(self)
-        
-        # self.stopFlag = Event()
-        # thread = MyThread(self.stopFlag, self)
-        # thread.start()
 
-        
-    def create_lock(self):
-        for key in self.page_directory:
-            if (key[0] == 'b'):
-                self.lock[key] = None
-                
-    def merge(self):
+        self.thrd = RepeatedTimer(.1, self.__merge)
+
+    def __merge(self):
         print("merge is happening")
         for page_range in self.page_range_list:
             recent_tail = page_range.recent_tail
             new_updated = page_range.recent_tail
-            
-            #check if there's any recent updates that haven't been merged
-            if page_range.has_updated != recent_tail: 
-                merge_base_page_list = copy.deepcopy(page_range.base_page_list)           
-                
+
+            # check if there's any recent updates that haven't been merged
+            if page_range.has_updated != recent_tail:
+                merge_base_page_list = copy.deepcopy(page_range.base_page_list)
+
                 for i in range(len(merge_base_page_list)):
                     merge_base_page = merge_base_page_list[i]
-                    
+
                     for j, rid in enumerate(merge_base_page[RID_COLUMN], 0):
                         merge_base_page[TIMESTAMP_COLUMN][j] = int(time.time())
                         rid_tail = merge_base_page[INDIRECTION_COLUMN][j]
-                        
-                        #check if this record has any update or ever merged
-                        if (rid_tail != None and page_range.has_updated != None and int(rid_tail[1:]) <= int(page_range.recent_tail[1:])):
+
+                        # check if this record has any update or ever merged
+                        if (rid_tail != None and page_range.has_updated != None and int(rid_tail[1:]) <= int(
+                                page_range.recent_tail[1:])):
                             print('z')
-                            if (rid != -1):                            
-                                for k in range(self.table.num_columns):                                
+                            if (rid != -1):
+                                for k in range(self.table.num_columns):
                                     location_tail = self.page_directory[rid_tail]
                                     schema_encoding = merge_base_page[SCHEMA_ENCODING_COLUMN][j]
                                     if schema_encoding[k] == 1:
-                                        merge_base_page[4+k][j].write(page_range.tail_page_list[location_tail[0]].tail_page_list[location_tail[1]][4+k].read(location_tail[2]))
-                    
+                                        merge_base_page[4 + k][j].write(
+                                            page_range.tail_page_list[location_tail[0]].tail_page_list[
+                                                location_tail[1]][4 + k].read(location_tail[2]))
+
                     merge_base_page[INDIRECTION_COLUMN] = page_range.base_page_list[i][INDIRECTION_COLUMN].copy()
-                    merge_base_page[SCHEMA_ENCODING_COLUMN] = page_range.base_page_list[i][SCHEMA_ENCODING_COLUMN].copy()
-                page_range.base_page_list = merge_base_page_list 
+                    merge_base_page[SCHEMA_ENCODING_COLUMN] = page_range.base_page_list[i][
+                        SCHEMA_ENCODING_COLUMN].copy()
+                page_range.base_page_list = merge_base_page_list
                 page_range.has_updated = new_updated
                 self.db.write_page_range(page_range)
-       
-    # def timer(self):
-    #     threading.Thread(self.__merge(), daemon=True)
-        
-    # def printtest(self):
-    #     while (True):
-    #         print("hello")
-    #         time.sleep(4)
-class MyThread(Thread):
-    def __init__(self, event, table):
-        Thread.__init__(self)
-        self.stopped = event
-        self.table : Table = table
-    def run(self):
-        while not self.stopped.wait(0.5):
-            self.table.merge()
+
+    def printtest(self):
+        print("hello")
